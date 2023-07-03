6faeb18b409f58699bf925e0acf118b44467e507
chamithgithub <dchamith4@gmail.com>
2023-07-03 19:01:01 +0530

    content update

--- e-commerce-app/src/routers/AppRouter.jsx
+++ e-commerce-app/src/routers/AppRouter.jsx
@@ -1,26 +1,38 @@
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter,Route,RouterProvider,Routes, createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layout/MainLayout'
 import User from '../user/User'
 import Home from '../pages/home/Home'
 import Login from '../pages/login/Login'
 import UserProtected from './UserProtected'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<MainLayout />,
+    children:[
+    {
+      index: true,
+      element: <Home />
+    },
+    {
+      element:<UserProtected />,
+      children:[
+        {
+          path:'user',
+          element:<User />,
+        },
+      ],
+    },
+  ],
+  },
+  {
+    path:'/login',
+    element:<Login />
+  },
+]);
+
 const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route element={<UserProtected />}>     
-          <Route path="user" element={<User />} />
-        </Route>
-      </Route>
-      <Route path="/login" >
-        <Route index element={<Login />} />
-      </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
                                         
 export default AppRouter;
