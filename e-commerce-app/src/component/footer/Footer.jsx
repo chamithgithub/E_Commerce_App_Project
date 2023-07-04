2a35ba6b0c18540d66791b63e13e0b333e4d656e
chamithgithub <dchamith4@gmail.com>
2023-07-04 23:38:57 +0530

    content update

--- e-commerce-app/src/component/footer/Footer.jsx
+++ e-commerce-app/src/component/footer/Footer.jsx
@@ -2,12 +2,19 @@
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import { IconButton } from '@mui/material';
 import AppsIcon from '@mui/icons-material/Apps';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 z-[100] left-0 w-full p-5 bg-[#efc3c3] flex items-center justify-between drop-shadow-header-shadow">
-    <IconButton> <HomeOutlinedIcon /></IconButton>
-    <IconButton> <AppsIcon /></IconButton>
+    <footer className="fixed bottom-0 z-[100] left-0 w-full py-1 px-2 bg-[#efc3c3] flex items-center justify-between drop-shadow-header-shadow">
+    <IconButton sx={{
+      color:'red',
+      padding:'4px',
+      borderRadius:'5px',
+      
+    }}> <HomeOutlinedIcon /><p>Home</p></IconButton>
+    <IconButton> <AppsIcon /><p>Category</p></IconButton>
+    <IconButton> <PersonOutlineIcon /><p>Profile</p></IconButton>
     </footer>
   )
 }
