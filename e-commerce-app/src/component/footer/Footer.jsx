d04cb0659b188ea24aa2c110ad6dc5a51523eb94
chamithgithub <dchamith4@gmail.com>
2023-07-23 23:15:42 +0530

    content u

--- e-commerce-app/src/component/footer/Footer.jsx
+++ e-commerce-app/src/component/footer/Footer.jsx
@@ -1,22 +1,43 @@
-
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import { IconButton } from '@mui/material';
-import AppsIcon from '@mui/icons-material/Apps';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { IconButton } from "@mui/material";
+import AppsIcon from "@mui/icons-material/Apps";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 z-[100] left-0 w-full py-1 px-2 bg-[#efc3c3] flex items-center justify-between drop-shadow-header-shadow">
-    <IconButton sx={{
-      color:'red',
-      padding:'4px',
-      borderRadius:'5px',
-      
-    }}> <HomeOutlinedIcon /><p>Home</p></IconButton>
-    <IconButton> <AppsIcon /><p>Category</p></IconButton>
-    <IconButton> <PersonOutlineIcon /><p>Profile</p></IconButton>
+      <IconButton>
+        {" "}
+        <HomeOutlinedIcon />
+        <p>Home</p>
+      </IconButton>
+      <IconButton>
+        {" "}
+        <AppsIcon />
+        <p>Category</p>
+      </IconButton>
+      <IconButton>
+        {" "}
+        <PersonOutlineIcon />
+        <p>Profile</p>
+      </IconButton>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+
+const FooterIcon = ({ Icon }) => (
+  <IconButton
+           sx={{
+          color: "red",
+          padding: "4px",
+          borderRadius: "5px",
+        }}
+  >
+    <div>
+      <Icon />
+      <p className="text-xs">Home</p>
+    </div>
+  </IconButton>
+);
--- e-commerce-app/src/component/header/Header.jsx
+++ e-commerce-app/src/component/header/Header.jsx
@@ -1,28 +1,40 @@
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import { IconButton } from '@mui/material';
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { IconButton } from "@mui/material";
 
 const Header = () => {
-  return <header className="fixed top-0 z-[100] left-0 w-full p-5 bg-[#efc3c3] flex items-center justify-between drop-shadow-header-shadow" >
+  return (
+    <header className="fixed top-0 z-[100] left-0 w-full p-5 bg-[#efc3c3] flex items-center justify-between drop-shadow-header-shadow">
       {/* header left*/}
-        <div className="flex items-center">
-        <IconButton sx={{color:'red',}}>
-        <MenuIcon sx={{
-          color:'blue',
-
-          }}/>
+      <div className="flex items-center">
+        <IconButton sx={{ color: "red" }}>
+          <MenuIcon
+            sx={{
+              color: "blue",
+            }}
+          />
+        </IconButton>
+        <h1 className="ml-2 text-sm font-semibold">
+          Chamith <span className="text-[#ff2727]">Dev</span>
+        </h1>
+        <div className="overflow-hidden ml-2 flex items-center rounded-full bg-[#e6dfd7]">
+          <input
+            type="text"
+            placeholder="Search"
+            className="hidden sm:inline-block outline-none ml-1 p-2 w-[200px] bg-inherit font-semibold text-sm"
+          />
+          <IconButton>
+            <SearchIcon className="text-gray-400" />{" "}
           </IconButton>
-        <h1 className="ml-2 text-sm font-semibold">Chamith <span className="text-[#ff2727]">Dev</span></h1>   
-       <div className="overflow-hidden ml-2 flex items-center rounded-full bg-[#e6dfd7]">
-        <input type="text" placeholder="Search" className="hidden sm:inline-block outline-none ml-1 p-2 w-[200px] bg-inherit font-semibold text-sm"/>
-        <IconButton><SearchIcon className="text-gray-400"/> </IconButton>
-       </div>
-       </div>
+        </div>
+      </div>
       {/* header right*/}
-      <IconButton><ShoppingCartOutlinedIcon sx={{color:'red',}} /></IconButton>
-      
-    </header>;
+      <IconButton>
+        <ShoppingCartOutlinedIcon sx={{ color: "red" }} />
+      </IconButton>
+    </header>
+  );
 };
 
 export default Header;
