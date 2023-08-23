a1978d47ed213fb98dcc5dec0c75b49eed1ab770
chamithgithub <dchamith4@gmail.com>
2023-08-23 23:04:51 +0530

    Swiper grid add

--- e-commerce-app/src/pages/home/Product.jsx
+++ e-commerce-app/src/pages/home/Product.jsx
@@ -4,7 +4,28 @@ const Product = ({title}) => {
   return (
     <section>
       <h1>{title}</h1>
-      
+      <Swiper
+        slidesPerView={3}
+        grid={{
+          rows: 2,
+        }}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Grid, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </section>
   )
 }
